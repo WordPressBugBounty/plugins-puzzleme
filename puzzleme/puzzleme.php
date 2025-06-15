55a41b06b2a4eb5599c90d0d959d1fc3eab2db51
Sean Murphy <sean@iamseanmurphy.com>
2025-06-15 00:43:52 +0000

    1.2.0

---
+++ puzzleme/puzzleme.php
@@ -0,0 +1,140 @@
+<?php
+/*
+   Plugin Name: PuzzleMe for WordPress
+   Version: 1.2.0
+   Description: Embed PuzzleMe puzzles in your posts and pages with a shortcode
+   Author: Amuse Labs
+   Author URI: https://www.amuselabs.com/
+   License: GPLv2 or later
+   License URI: https://www.gnu.org/licenses/gpl-2.0.html
+   */
+
+function load_puzzle_scripts() {
+
+    global $puzzleme_base_path;
+
+    $footer_script_HTML = '
+    
+    <script nowprocket data-no-minify="1" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
+    <script nowprocket data-no-minify="1" id="pm-script" src="' . $puzzleme_base_path . 'js/puzzleme-embed.js"></script>
+    <script nowprocket>
+    PM_Config.PM_BasePath = "' . $puzzleme_base_path . '";
+    </script>
+
+    ';
+    echo($footer_script_HTML);
+}
+
+
+function puzzleme_iframe_generator($attributes)
+{
+
+    global $puzzleme_base_path;
+
+    if (isset($attributes['basepath'])) {
+        $puzzleme_base_path = esc_url_raw($attributes['basepath']);
+    } else if (defined('PuzzleMe_BasePath')) {
+        $puzzleme_base_path = PuzzleMe_BasePath;
+    } else {
+        $puzzleme_base_path = 'https://puzzleme.amuselabs.com/pmm/';
+    }
+
+    add_action('wp_footer', 'load_puzzle_scripts');
+
+    $allowedHTMLtags = array(
+        'div' => array(
+            'class' => true,
+            'data-id' => true,
+            'data-set' => true,
+            'data-puzzletype' => true,
+            'data-height' => true,
+            'data-mobileMargin' => true,
+            'data-embedparams' => true,
+            'data-page' => true,
+            'style' => true
+        ),
+        'script' => array(
+            'id' => true,
+            'src' => true,
+            'nowprocket' => true
+        ),
+        'a' => array(
+            'href' => true,
+            'target' => true,
+            'style' => true,
+            'title' => true,
+            'rel' => true
+        )
+    );
+
+    $valid_embed_types = array('crossword', 'sudoku', 'wordsearch', 'quiz', 'krisskross', 'wordf', 'codeword', 'wordrow', 'jigsaw', 'date-picker');
+
+    $embed_html = '
+
+        <div class="pm-embed-div" $id data-set="$set" $embed_type data-height="700px" data-mobileMargin="10px" $embed_params></div>
+
+        ';
+    
+    $embed_html_attributions = '
+        <div style="position: relative; text-align: center;">
+            <div class="pm-embed-div" $id data-set="$set" $embed_type data-height="700px" data-mobileMargin="10px" $embed_params></div>
+            <div class="pm-attribution-div" style="font-family: sans-serif; font-size: 12px; color:#666666; padding-top: 5px; width: 100%;">$attribution_text</div>
+        </div>
+        ';
+
+    $embed_variables = array(
+        '$embed_type' => '',
+        '$set' => '',
+        '$embed_params' => '',
+        '$id' => '',
+        '$attribution_text' => '',
+    );
+
+    if (empty($attributes)) {
+        return "Parameters are missing in the shortcode. Please copy the correct shortcode from your PuzzleMe dashboard.";
+    } else {
+        if (isset($attributes['set'])) {
+            if (isset($attributes['type'])) {
+                if (in_array($attributes['type'], $valid_embed_types)) {
+
+                    $embed_variables['$embed_type'] = sanitize_text_field($attributes['type']);
+                    $embed_variables['$set'] = sanitize_text_field($attributes['set']);
+                    if (isset($attributes['embedparams'])) {
+                        $embed_variables['$embed_params'] = 'data-embedparams="embed=wp&' . sanitize_text_field($attributes['embedparams']) . '"';
+                    } else {
+                         $embed_variables['$embed_params'] = 'data-embedparams="embed=wp"' ;
+                    }
+                    if ($attributes['type'] != 'date-picker') {
+                        $embed_variables['$embed_type'] = 'data-puzzleType="' . sanitize_text_field($attributes['type']) . '"';
+                        if (isset($attributes['id'])) {
+                            $embed_variables['$id'] = 'data-id="' . sanitize_text_field($attributes['id']) . '"';
+                        } else {
+                            return "Puzzle ID is missing in the shortcode. Please use the correct shortcode from your PuzzleMe dashboard.";
+                        }
+                    } else {
+                        $embed_variables['$embed_type'] = 'data-page="date-picker"';
+                    }
+                    if (isset($attributes['attribution'])) {
+                        $embed_variables['$attribution_text'] = $attributes['attribution'];
+                        return wp_kses(strtr($embed_html_attributions, $embed_variables), $allowedHTMLtags);
+                    } else {
+                        return wp_kses(strtr($embed_html, $embed_variables), $allowedHTMLtags);
+                    }
+
+                } else {
+                    return "Invalid puzzle type is present in the shortcode. Please use the correct shortcode from your PuzzleMe dashboard.";
+                }
+
+            } else {
+                return "Puzzle type is missing in the shortcode. Please use the correct shortcode from your PuzzleMe dashboard.";
+            }
+
+        } else {
+            return "Set is missing in the shortcode. Please use the correct shortcode from your PuzzleMe dashboard.";
+        }
+    }
+}
+
+add_shortcode('puzzleme', 'puzzleme_iframe_generator');
+
+?>
---
+++ puzzleme/readme.txt
@@ -0,0 +1,44 @@
+=== PuzzleMe for WordPress ===
+Contributors: amuselabs
+Tags: crossword, sudoku, quiz, puzzles, games
+Requires at least: 3.9
+Tested up to: 6.8
+Stable tag: 1.2.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ 
+Embed a PuzzleMe™ crossword, sudoku, word search, codeword, kriss kross, WordroW, jigsaw or quiz on your website, with just a shortcode
+
+== Description ==
+
+PuzzleMe™ by Amuse Labs is the leading digital platform to create, edit, manage, publish and track smart games like crossword, sudoku, word search, jigsaw, and quiz among others. Use our WordPress plugin to publish your puzzles with a shortcode.
+
+Start creating and publishing your puzzles in 3 simple steps :
+1. Log in to <a target="_blank" href="https://puzzleme.amuselabs.com/pmm/login">PuzzleMe</a>.
+2. Create a puzzle and copy the WordPress shortcode from the "Preview & Publish" page.
+3. Paste the shortcode in your page or post and hit Publish.
+
+PuzzleMe is a registered trademark of Amuse Labs. By using PuzzleMe, you agree to our <a target="_blank" href="https://amuselabs.com/tos/">terms of service</a> and <a target="_blank" href="https://amuselabs.com/privacy-policy/">privacy policy</a>. 
+
+== Frequently Asked Questions ==
+
+= Which puzzles do you support? =
+
+PuzzleMe supports many smart games including crossword, word search, sudoku, jigsaw, WordroW, jigsaw, and quiz. See our <a target="_blank" href="https://amuselabs.com/games/">game pages</a> for a full list of puzzles. 
+
+= Do I need to create an account? =
+
+You can initially create a game without logging in, however, to save your progress, edit, manage, and publish the puzzle, you will need to be logged in to PuzzleMe. For personal and non commercial usage, please use one of the available social login methods on <a target="_blank" href="https://puzzleme.amuselabs.com/pmm/login">this page</a>. 
+
+= Do puzzle solvers need to create an account? =
+
+Your puzzle solvers do not need to create an account with PuzzleMe.
+
+= What is your pricing? =
+
+PuzzleMe's WordPress plugin is free to use. Please visit our <a href="https://amuselabs.com/pricing" target="_blank">pricing page</a> for subscription plans and feature availability.
+
+= What if I have other questions about PuzzleMe? =
+
+Please <a href="https://amuselabs.com/contact-us" target="_blank">contact us</a>.
